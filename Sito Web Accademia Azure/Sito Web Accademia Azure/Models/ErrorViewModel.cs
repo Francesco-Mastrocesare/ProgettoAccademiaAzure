a7fc2ba05477a35b49e672d0cc905ce967daf190
Francesco-Mastrocesare <francesco.mastrocesare@gmail.com>
2018-12-05 14:29:06 +0100

    push azure

---
+++ Sito Web Accademia Azure/Sito Web Accademia Azure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sito_Web_Accademia_Azure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
